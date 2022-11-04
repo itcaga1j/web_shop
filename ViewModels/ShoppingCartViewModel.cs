c7775f9f8080b737e63bcd03f0e754704405b00f
itcagalj <92359665+itcagalj@users.noreply.github.com>
2022-11-04 17:04:46 +0100

    Add files via upload

---
+++ ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MyPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPieShop.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
